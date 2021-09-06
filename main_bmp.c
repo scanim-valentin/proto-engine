3f59bd763f46e62150639db1b856142f8d00e550
scanim-valentin <valentin.guerbette@hotmail.fr>
2021-09-06 19:33:16 +0200

    Solved the following issues: -"unsigned long" is 8 bytes long on 64 bits machines -structures needed to be packed to avoid padding (this might not be the case in the future, but for now I need to now the exact size of the BMP structure)
    Issues to solve:
    -the generated BMP file does not match the expectation (see reference.bmp)

--- main_bmp.c
+++ main_bmp.c
@@ -1,29 +1,31 @@
 #include "bmp_management.h"
 #include <stdio.h>
 
+#define NB_PIXELS 9
 int main(){
 
-    Header theHeader = {0x4C42,BMP_size(9),0,0};
-    printf("Taille du fichier = %u\n",BMP_size(9));
+    Header theHeader = {0x4D42,(unsigned long)(54+3*NB_PIXELS),0,54};
+
+    printf("Taille du fichier = %d\n",theHeader.FileSize);
     InfoHeader theInfoHeader = {40,3,3,1,24,0,0,1000,1000,16777216,0};
 
     PixelDataElement p1 = {0,0,0};
-    PixelDataElement p2 = {0,0,1};
-    PixelDataElement p3 = {0,1,0};
+    PixelDataElement p2 = {0,0,254};
+    PixelDataElement p3 = {0,254,0};
 
-    PixelDataElement p4 = {0,1,1};
-    PixelDataElement p5 = {1,0,0};
-    PixelDataElement p6 = {1,0,1};
+    PixelDataElement p4 = {0,254,254};
+    PixelDataElement p5 = {254,0,0};
+    PixelDataElement p6 = {254,0,254};
 
-    PixelDataElement p7 = {1,1,0};
-    PixelDataElement p8 = {1,1,1};
+    PixelDataElement p7 = {254,254,0};
+    PixelDataElement p8 = {254,254,254};
     PixelDataElement p9 = {0,0,0};
 
     PixelDataElement pTab[9] = {p1,p2,p3,p4,p5,p6,p7,p8,p9};
 
     BitMap theBMP = {theHeader,theInfoHeader,pTab};
     FILE * fileBMP = fopen("test.bmp","wb");
-    fwrite(&theBMP,theBMP.infoHeader.Size,1,fileBMP);
+    fwrite(&theBMP,81,1,fileBMP);
     fclose(fileBMP);
     return 0;
 }
