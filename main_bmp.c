5e1bc19fceac4d18e6d1f2d94d6d60c02638633d
scanim-valentin <valentin.guerbette@hotmail.fr>
2021-10-20 17:16:05 +0200

    qsdqsdqsd

--- main_bmp.c
+++ main_bmp.c
@@ -4,28 +4,34 @@
 #define NB_PIXELS 9
 int main(){
 
-    Header theHeader = {0x4D42,(unsigned long)(54+3*NB_PIXELS),0,54};
+    Header theHeader = {0x4D42,(unsigned long)(54+3*NB_PIXELS),0,45};
 
     printf("Taille du fichier = %d\n",theHeader.FileSize);
-    InfoHeader theInfoHeader = {40,3,3,1,24,0,0,1000,1000,16777216,0};
+    InfoHeader theInfoHeader = {40,3,3,1,24,0,0,10,10,16777216,0};
 
     PixelDataElement p1 = {0,1,2};
     PixelDataElement p2 = {3,4,5};
     PixelDataElement p3 = {6,7,8};
 
     PixelDataElement p4 = {9,10,11};
-    PixelDataElement p5 = {13,14,15};
-    PixelDataElement p6 = {16,17,18};
+    PixelDataElement p5 = {12,13,14};
+    PixelDataElement p6 = {15,16,17};
 
-    PixelDataElement p7 = {19,20,21};
-    PixelDataElement p8 = {22,23,24};
-    PixelDataElement p9 = {25,26,27};
+    PixelDataElement p7 = {18,19,20};
+    PixelDataElement p8 = {21,22,23};
+    PixelDataElement p9 = {24,25,27};
 
     PixelDataElement pTab[9] = {p1,p2,p3,p4,p5,p6,p7,p8,p9};
 
-    BitMap theBMP = {theHeader,theInfoHeader,pTab};
+    //BitMap theBMP = {theHeader,theInfoHeader,pTab};
     FILE * fileBMP = fopen("test.bmp","wb");
-    fwrite(&theBMP,81,1,fileBMP);
+    //fwrite(&theBMP,91,1,fileBMP);
+    fwrite(&theHeader,sizeof(Header),1,fileBMP);
+    fwrite(&theInfoHeader,sizeof(InfoHeader),1,fileBMP);
+    for(int i = 0 ; i < 10 ; i++){
+        printf("i = %d : %p | R %d G %d B %d \n",i,&(pTab[i]),pTab[i].Red,pTab[i].Blue,pTab[i].Green);
+        fwrite(&(pTab[i]),sizeof(PixelDataElement),1,fileBMP);
+    }
     fclose(fileBMP);
     return 0;
 }
